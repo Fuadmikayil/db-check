b8150a1d8670124e206d5c26cb0da2f9c3ed9cf9
Fuadmikayil <mikayilov2006@gmail.com>
2025-04-26 10:32:46 +0400

    Initial commit from Create Next App

---
+++ app/layout.jsx
@@ -0,0 +1,18 @@
+import "./globals.css";
+
+
+export const metadata = {
+  title: "Demo",
+  description: "Demo",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body
+      >
+        {children}
+      </body>
+    </html>
+  );
+}
