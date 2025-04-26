8591e7b7abe1886e98377a0772a13bc83acc371d
Fuadmikayil <mikayilov2006@gmail.com>
2025-04-26 18:43:51 +0400

    middleware

--- middleware.js
+++ middleware.js
@@ -1,15 +1,27 @@
+//middleware
 import { NextResponse } from "next/server";
+import { verifyJWT } from "./lib/auth";
 
-export function middleware(request) {
-  const { pathname } = request.nextUrl;
-  if (pathname === "/userPanel") {
-    const token = request.cookies.get("token")?.value;
-    console.log(token);
+export async function middleware(request) {
     
+  const { pathname } = request.nextUrl;
+
+  if (pathname === "/userpanel") {
+    const token = request.cookies.get("token")?.value;
+
     if (!token) {
       return NextResponse.redirect(new URL("/signin", request.url));
     }
+
+    try {
+        await verifyJWT(token);
+        // if valid, let the request continue
+        return NextResponse.next();
+      } catch (err) {
+        // expired / malformed → redirect to signin
+        return NextResponse.redirect(new URL("/signin", request.url));
+      }
+   
   }
 
-  return NextResponse.next();
 }
